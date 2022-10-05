7075c0f585b6bbcfe993c4c9df185ff7e5815dde
Espen G <espen_gud@msn.com>
2022-10-05 19:08:44 +0200

    Added tsx/jsx to config.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["index.html", "./src/**/*.{html,js,ts}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
